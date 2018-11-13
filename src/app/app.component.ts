b80127e95b68663b4271a301e53da02cf75b9031
Tianwei Liu <TianweiLiu2012@gmail.com>
2018-11-12 22:36:47 -0500

    Added custom validator on the input string of the date picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, Validators} from '@angular/forms';
 import {MomentDateAdapter} from '@angular/material-moment-adapter';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 import * as moment from 'moment';
@@ -27,7 +27,41 @@ export const MY_FORMATS = {
 })
 export class AppComponent {
   title = 'datepicker-input-check';
-  date = new FormControl(moment());
+  date = new FormControl(
+    'date', [
+      Validators.required
+    ]
+  );
   // minDate is the start of today.
   minDate = new Date(new Date().setHours(0, 0, 0, 0));
+
+  getErrorMessage(pickerInput: string): string {
+    if (!pickerInput || pickerInput === '' ) {
+      return 'Please choose a date.';
+    }
+    return this.isMyDateFormat(pickerInput);
+  }
+  /**
+   * An example of custom validator on a date string.
+   * Valid date is a string, which is:
+   * 1, in the form of YYYY-MM-DD
+   * 2, later than today
+   * 3, not an invalid value like 2018-20-81
+   * @param date a date string
+   */
+  isMyDateFormat(date: string): string {
+    if (date.length !== 10) {
+      return 'Invalid input: Please input a string in the form of YYYY-MM-DD';
+    } else {
+      const da = date.split('-');
+      if (da.length !== 3 || da[0].length !== 4 || da[1].length !== 2 || da[2].length !== 2) {
+        return 'Invalid input: Please input a string in the form of YYYY-MM-DD';
+      } else if (moment(date).isValid()) {
+        return 'Invalid date: Please input a date no later than today';
+      } else if (!moment(date).isValid()) {
+        return 'Invalid date: Please input a date with a valid month and date.';
+      }
+    }
+    return 'Unknown error.';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatDatepickerModule, MatFormFieldModule, MatInputModule } from '@angular/material';
@@ -12,7 +12,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatInputModule
